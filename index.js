a633d423e20ac85a9195fe316d6371f1a762e66b
ibraa9 <essaidi.ib@hotmail.com>
2021-12-22 13:06:26 +0100

    animations au chargement de la page ajoutés, avancé sur le texte

---
+++ index.js
@@ -0,0 +1,42 @@
+class Animation {
+
+    constructor() {
+        this.néon = false
+    }
+
+    launchAnimations() {
+        var i = 0
+        var speed = [1200, 800, 1000, 500, 300, 300, 300, 400, 700, 1200]
+        this.néonChange(i, speed) 
+    }
+
+    néonChange(i, speed) {
+        var header = document.querySelector("header")
+        if(i<=10) {
+            setTimeout(() => {
+                if (this.néon == false) {
+                    header.style.textShadow = "0 0 4px rgb(240, 240, 240)"
+                    header.style.boxShadow = "rgb(150, 150, 150) 0 0 6px 2px inset, rgb(150, 150, 150) 0 0 6px 2px"
+                    this.néon = true
+                } 
+                else {
+                    header.style.textShadow = "";
+                    header.style.boxShadow = "";
+                    this.néon = false
+                }
+                i++
+                this.néonChange(i, speed)
+            },speed[i])
+        }
+        else {
+            header.style.boxShadow = "#646464 0 0 6px 2px inset, #646464 0 0 6px 2px"
+            header.style.textShadow = "0 0 4px rgb(200, 200, 200)"
+        }
+    }
+} 
+
+var animation = new Animation()
+window.onload=() => {
+    setTimeout(() => {animation.launchAnimations()},3200)
+}
+
